853ba5092be921ace6ac55b317a29a7f2105fb27
Thomas Pizon <pizon.thomas@gmail.com>
2012-12-04 19:28:33 +0100

    L'ecritures de fichiers et l'extraction de fichiers est fonctionnelle

--- src/processing.c
+++ src/processing.c
@@ -1,8 +1,12 @@
-#include "processing.h"
-
 #include <stdbool.h>
 #include <stdio.h>
+#include <assert.h>
+#include <sys/stat.h>
 #include <dirent.h>
+
+#include "processing.h"
+
+bool VERBOSE = false;
 
 /**
  * Écrit le contenu d'un dossier (récursivement) fdans l'archive.
@@ -15,6 +19,7 @@
  *
  * @TODO peut être trouver un moyen d'éviter les appels récursifs.
  */
+ /*
 void write_directory(FILE * archive, char * directory_name) {
     DIR * directory = opendir(directory_name);
 
@@ -25,12 +30,8 @@ void write_directory(FILE * archive, char * directory_name) {
         // Écriture du header du dossier
         // TODO: ajouter des information supplémentaires au header.
         Header header = create_header(directory_name,
-                                      NULL,
-                                      NULL,
-                                      NULL,
-                                      NULL,
-                                      NULL,
-                                      NULL,
+                                      0664,
+                                      12000,
                                       DIRECTORY_TYPE,
                                       NULL);
 
@@ -39,19 +40,16 @@ void write_directory(FILE * archive, char * directory_name) {
 
         // Analyse des entrées du dossier.
         struct dirent *entry;
-        while (entry = readdir(directory)) {
+        while ((entry = readdir(directory))) {
             printf("Entrée %s du dossier %s\n", entry->d_name, directory_name);
 
-            switch (entry->d_type) {
+            if (entry->d_type == DT_REG) {
                 // Fichier normal
-                case DT_REG:
-                    write_file(archive, entry->d_name);
-                    break;
-
+                write_file(archive, entry->d_name);
+            }
+            else if (entry->d_type == DT_DIR) {
                 // Dossier
-                case DT_DIR:
-                    write_directory(archive, entry->d_name);
-                    break;
+                write_directory(archive, entry->d_name);
             }
         }
     }
@@ -59,4 +57,91 @@ void write_directory(FILE * archive, char * directory_name) {
         fprintf(stderr, "Impossible d'ouvrir le dossier %s.\n", directory_name);
         exit(1);
     }
+}
+*/
+
+/**
+ * Écrit un fichier dans l'archive.
+ *
+ * @param archive L'archive dans laquelle sera écrite le fichier.
+ * @param file_name le nom (chemin) du fichier.
+ * @param verbose définit la verbosité de la sortie standard.
+ */
+void write_file(FILE * archive, char * file_name) {
+    struct stat file_stats;
+    if (stat(file_name, &file_stats) != 0) {
+        fprintf(stderr, "Le fichier %s n'existe pas.\n", file_name);
+        return;
+    }
+
+    if (VERBOSE) printf("Écriture du fichier %s\n", file_name);
+
+    Header file_header = create_header(file_name,
+                                       file_stats.st_size,
+                                       file_stats.st_mtime,
+                                       REGULAR_FILE_TYPE,
+                                       NULL);
+
+    if (VERBOSE) printf("Écriture du header\n");
+    write_header(archive, file_header);
+
+    FILE * input_file = fopen(file_name, "rb");
+    assert(input_file);
+
+    if (VERBOSE) printf("Écriture des données du fichier (%ldo).\n", file_stats.st_size);
+
+    long int bytes_written = 0;
+    char buffer[1024];
+    while (!feof(input_file)) {
+
+        // On lit les données dans le fichier source.
+        int n = fread(&buffer, sizeof(char), 1024, input_file);
+
+        // On écrit dans l'archive en vérifiant si la quantité de
+        // données lus et écrites sont les mêmes
+        assert(n == fwrite(&buffer, sizeof(char), n, archive));
+
+        bytes_written += n;
+        if (VERBOSE) printf("%ld octets écrits\n", bytes_written);
+    }
+
+    if (VERBOSE) printf("Fin de l'écriture du fichier %s\n", file_name);
+
+    fflush(archive);
+}
+
+/**
+ * Extrait un fichier de l'archive.
+ * Il aura le même nom nom et la même position dans l'arborescence
+ * que décrit dans son header. Il est important de noter que pour le bon
+ * fonctionnement de cette fonction, le curseur de l'archive doit être
+ * situé juste après le header qui décrit le fichier. Ce qui normalement
+ * le cas après l'éxécution de read_header.
+ *
+ * @param archive l'archive de laquelle extraire le fichier.
+ * @param file_header Le header correspondant au fichier à extraire.
+ */
+void extract_file(FILE * archive, Header file_header) {
+    char * file_name = header_file_name(file_header);
+    off_t file_size = header_file_size(file_header);
+
+    FILE * output_file = fopen(file_name, "wb");
+    assert(output_file);
+
+    if (VERBOSE) printf("Extraction du fichier %s (%ldo)\n", file_name, file_size);
+
+    long int bytes_written = 0;
+    char buffer[1024];
+    while(bytes_written < file_size) {
+        int n = fread(&buffer, sizeof(char), 1024, archive);
+        assert(n == fwrite(&buffer, sizeof(char), n, output_file));
+
+        bytes_written += n;
+        if (VERBOSE) printf("%ld octets écrits\n", bytes_written);
+    }
+
+    if (VERBOSE) printf("Extraction de %s terminée.\n", file_name);
+
+    fflush(output_file);
+    fclose(output_file);
 }
--- src/processing.h
+++ src/processing.h
@@ -1,25 +1,19 @@
 #ifndef INCLUDE_PROCESSING_H
 #define INCLUDE_PROCESSING_H
 
-#include <stdbool.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <dirent.h>
 
 #include "header.h"
 
-bool VERBOSE = false;
+extern bool VERBOSE;
 
 void write_directory(FILE * archive, char * directory_name);
-void list_directory(FILE * archive, Header directory_header);
 void extract_directory(FILE * archive, Header directory_header);
 
 void write_file(FILE * archive, char * file_name);
-void list_file(FILE * archive, Header file_header);
 void extract_file(FILE * archive, Header file_header);
-
-int create(char * filename, char * file_names[]);
-int list(char * filename);
-int add(char * filename, char * file_names[], bool update);
-int extract(char * filename);
 
 #endif
